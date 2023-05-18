6b750d9320a15048f57e03e2be9d6b96fca3ccd9
ethan7lesar <ethanlesar11@gmail.com>
2023-05-18 09:00:52 +0200

    Method Update

--- Script/code.js
+++ Script/code.js
@@ -1,9 +1,11 @@
 let subject = [ 'Python', 'C++',
     'VB.Net', 'Javascript', 'Assembly'];
-
+let size = subject.length;
 // console.log(subject.pop());
 
-console.log(subject[subject.length-1]);
+console.log(subject[size - 1]);
 
-console.log(subject[3]);
+console.log(subject[2]);
+
+// document.write(subject[size-1]);
 
